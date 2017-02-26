a55c32f313b167315ec80b9ebacec578e6d92db9
Ayush Goyal <goyal.ayush55@gmail.com>
2017-02-26 12:38:06 +0530

    order with email feature added

--- order_app/app/src/main/java/com/example/my/userinput/MainActivity.java
+++ order_app/app/src/main/java/com/example/my/userinput/MainActivity.java
@@ -6,11 +6,15 @@
  */
 package com.example.my.userinput ;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //import com.example.my.userinput.R;
 
@@ -29,21 +33,41 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    int quantity = 0;
+    int quantity = 2;
     public void submitOrder(View view) {
-        int price = calculatePrice();
+
+
         CheckBox whippedcream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);  //created a Whipped cream object
         boolean cream = whippedcream.isChecked();  //check if whipped cream is true or not
         CheckBox chocolate = (CheckBox) findViewById(R.id.choco_checkbox);
         boolean choco = chocolate.isChecked();
-        display(quantity);
-        createOrderSummary(price,cream,choco);
+
+        int price = calculatePrice(cream,choco);
+
+        EditText name = (EditText) findViewById(R.id.name_view);   //getting name from user
+        String custumerName = name.getText().toString();            //storing it into variable
+
+     //   display(quantity);
+
+        // creating intent for using gmail app
+       composeEmail(summary(price,cream,choco,custumerName),custumerName);
     }
     public void increament(View view){
+        if (quantity>100)
+        {
+            Toast.makeText(this,"You cannot have more than 100 cups",Toast.LENGTH_SHORT).show();// sending a toast msg warning the user
+            return;
+        }
         quantity++;
         display(quantity);
     }
     public void decrement(View view){
+
+        if (quantity<=1)
+        {
+            Toast.makeText(this,"You cannot have less than 1 cup ",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity--;
         display(quantity);
     }
@@ -64,22 +88,52 @@ public class MainActivity extends AppCompatActivity {
         orderTextView.setText(message);
     }
 
-    private int calculatePrice()
+    private int calculatePrice(boolean cream,boolean choco)
     {
-        return quantity * 5;
+        int basePrice = 5;
+        if (cream)
+            basePrice+=1;
+        if (choco)
+            basePrice+=2;
+        return (quantity * basePrice);
     }
-    private void createOrderSummary(int price,boolean cream,boolean choco){
-
-        displayMessage ("add whipped cream?"+ cream +
+    /*
+    return a string for order summary
+     */
+    private String summary (int price,boolean cream,boolean choco,String name)
+    {
+        String order =    "\nadd whipped cream?"+ cream +
                 "\nadd whipped cream?"+ choco +
-                "\nName: ayushGoyal" +
+                "\nName: " + name +
                 "\nQuantity =" + quantity +
-        "\nTotal = " + price +
-        "\nThank you");
+                "\nTotal = " + price +
+                "\nThank you";
+        return order;
     }
-  /*  private void whippedcream(View view)
-    {
+    /*
+       For email app intent
+     */
+    public void composeEmail(String body,String name) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Order for"+ name);
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+   /*
+   display order summary in the app only
+    */
+    private void createOrderSummary(int price,boolean cream,boolean choco,String name){
 
+        displayMessage (
+
+                "\nadd whipped cream?"+ cream +
+                "\nadd whipped cream?"+ choco +
+                "\nName: " + name +
+                "\nQuantity =" + quantity +
+                "\nTotal = " + price +
+                "\nThank you");
     }
-*/
 }
